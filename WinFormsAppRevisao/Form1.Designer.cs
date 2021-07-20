db16e150e30e14d17115ca3a1f683e5e53701e5d
TIAGO SILVA <frost.tiago@gmail.com>
2021-07-20 16:01:01 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsAppRevisao/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace WinFormsAppRevisao
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_segundo_numero = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_primero_numero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_segundo_numero);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_primero_numero);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 162);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculadora de Revisão";
+            // 
+            // txt_segundo_numero
+            // 
+            this.txt_segundo_numero.Location = new System.Drawing.Point(7, 91);
+            this.txt_segundo_numero.Name = "txt_segundo_numero";
+            this.txt_segundo_numero.Size = new System.Drawing.Size(215, 23);
+            this.txt_segundo_numero.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Segundo Número";
+            // 
+            // txt_primero_numero
+            // 
+            this.txt_primero_numero.Location = new System.Drawing.Point(7, 42);
+            this.txt_primero_numero.Name = "txt_primero_numero";
+            this.txt_primero_numero.Size = new System.Drawing.Size(215, 23);
+            this.txt_primero_numero.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Primeiro Número";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SOMAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 188);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "WinForms .NET Core";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_segundo_numero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_primero_numero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WinFormsAppRevisao/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAppRevisao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double primeiro_num = Convert.ToDouble(txt_primero_numero.Text);
+            double segundo_num = Convert.ToDouble(txt_segundo_numero.Text);
+
+            double resultado = primeiro_num + segundo_num;
+
+            MessageBox.Show("A soma dos números é " + resultado);
+
+            MessageBox.Show(
+                "Resultado", 
+                "A soma dos números é " + resultado, 
+                MessageBoxButtons.OK, 
+                MessageBoxIcon.Information
+            );
+        }
+    }
+}
